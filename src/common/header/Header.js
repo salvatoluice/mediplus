e3bf21237cb95eb93623f9c98659e98cab8b2364
salvatoluice <salvatoluice5@gmail.com>
2023-03-12 10:36:00 -0700

    Develop navbar

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './header.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='app__header'>
+      <div className='app__navbar'>
+        <div className='navbar__logo'>
+            <Link to='/'>Medi<span>Plus</span></Link>
+        </div>
+        <div className='navbar__links'>
+            <ul>
+                <li>
+                    <Link>Home</Link>
+                </li>
+                <li>
+                    <Link>Services</Link>
+                </li>
+                <li>
+                    <Link>Gallery</Link>
+                </li>
+                <li>
+                    <Link>About Us</Link>
+                </li>
+                <li>
+                    <Link>Contact</Link>
+                </li>
+            </ul>
+        </div>
+        <div className='navbar__button'>
+            <button>
+                <span className='blinking-text'>Call Emergency!</span>
+            </button>
+        </div>
+      </div>
+      <div className='app__hero'></div>
     </div>
   )
 }
