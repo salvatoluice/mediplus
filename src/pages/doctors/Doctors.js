e4614b19fe71547291e4ffbc0f2ae05939f15f06
salvatoluice <salvatoluice5@gmail.com>
2023-03-15 10:17:02 +0300

    Complete Doctors Paage

--- src/pages/doctors/Doctors.js
+++ src/pages/doctors/Doctors.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './doctors.css'
+import { TiMessages } from 'react-icons/ti'
 
 const Doctors = () => {
   return (
@@ -14,16 +15,246 @@ const Doctors = () => {
         </div>
         <div className='line' />
         <div className='app__collection'>
-            <div className='doctors__management'>
-                <img src='' alt='doc' />
+            <h1>The management Team</h1>
+            <div className=' doctor doctors__management'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
             </div>
-            <div className='dictors__neurology'></div>
-            <div className='doctors__cardiology'></div>
-            <div className='doctors__dental'></div>
-            <div className='doctors__lab'></div>
-            <div className='dictors__eye'></div>
-            <div className='doctors__pulmonary'></div>
-            <div className='doctors__xray'></div>
+            <h1>Neurology Specialists</h1>
+            <div className='doctor dictors__neurology'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
+            <h1>Cardiology Specialists</h1>
+            <div className='doctor doctors__cardiology'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
+            <h1>Our Dental Doctors</h1>
+            <div className='doctor doctors__dental'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
+            <h1>Lab Specialists</h1>
+            <div className='doctor doctors__lab'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
+            <h1>Top Opticians</h1>
+            <div className='doctor doctors__eye'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
+            <h1>Pulmonary Experts</h1>
+            <div className='doctor doctors__pulmonary'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
+            <h1>X-Ray Proffessionals</h1>
+            <div className='doctor doctors__xray'>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+                <div className='doctors'>
+                    <img src='https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='doc' />
+                    <h2>Mirry Lumatete</h2>
+                    <h4>CEO & Founder</h4>
+                    <p>Exercitation excepteur voluptate sint et magna ut pariatur occaecat.</p>
+                    <h4>Santiago Medical Aschool, Cuba</h4>
+                    <h5>Cardiologist(MBBS, FCS)</h5>
+                    <button>Chat <TiMessages /></button>
+                </div>
+            </div>
         </div>
     </div>
   )
