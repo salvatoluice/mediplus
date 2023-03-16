5b582ef3befb18ef051a243d5e6b1fac5176358e
salvatoluice <salvatoluice5@gmail.com>
2023-03-16 11:57:37 +0300

    Configure firebase for auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
 import AppointmentForm from './components/appointment/AppointmentForm';
 import Pages from './components/Pages';
 import Doctors from './pages/doctors/Doctors';
+import Login from './pages/login/Login';
+import Signup from './pages/login/Signup';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -11,6 +15,7 @@ function App() {
         <Route exact path='/' element={<Pages />} />
         <Route exact path='/appointment' element={<AppointmentForm />} />
         <Route exact path='/doctors' element={<Doctors />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,96 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4Ctz0qUiJ2byUrTzV6N12qgJr3izFVW0",
+  authDomain: "mediplus-ed6f5.firebaseapp.com",
+  projectId: "mediplus-ed6f5",
+  storageBucket: "mediplus-ed6f5.appspot.com",
+  messagingSenderId: "637069123002",
+  appId: "1:637069123002:web:64e46545a465878b269183",
+  measurementId: "G-4J303HD15R"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
